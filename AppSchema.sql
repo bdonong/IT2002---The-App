f4d2e80afc7f88ea356eca1e10082725da390338
bdonong <106804158+bdonong@users.noreply.github.com>
2023-03-22 20:43:13 +0800

    Update AppSchema.sql
    - Removed password as a primary key as it was causing issues
    - Removed enum and changed to check statements
    - Changed serial to integer for predictable data insertion

--- AppSchema.sql
+++ AppSchema.sql
@@ -4,59 +4,66 @@
 
 ********************/
 
-CREATE TABLE IF NOT EXISTS user (
-  user_id SERIAL,
-  password VARCHAR(255) NOT NULL,
+/*
+DROP TABLE administrator;
+DROP TABLE review;
+DROP TABLE transaction;
+DROP TABLE booking;
+DROP TABLE property;
+DROP TABLE users;
+*/
+
+CREATE TABLE IF NOT EXISTS users (
+  user_id INTEGER PRIMARY KEY,
+  password_hash VARCHAR(255) NOT NULL,
   user_name VARCHAR(255) NOT NULL,
   email VARCHAR(255) UNIQUE NOT NULL,
   phone_number VARCHAR(20) UNIQUE NOT NULL,
-  gender VARCHAR(6) CHECK(gender = 'male' or gender = 'female')
-  age INTEGER,
-  PRIMARY KEY(user_id, password)
+  gender VARCHAR(6) CHECK(gender = 'male' or gender = 'female'),
+  age INTEGER NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS property (
-  property_id SERIAL PRIMARY KEY,
-  owner_id INTEGER REFERENCES user (user_id) DEFERRABLE,
-  address VARCHAR(255) UNIQUE NOT NULL,
-  type VARCHAR(255) NOT NULL,
+  property_id INTEGER PRIMARY KEY,
+  owner_id INTEGER REFERENCES users(user_id),
+  address VARCHAR(255) NOT NULL,
+  property_type VARCHAR(255) NOT NULL,
   num_rooms INTEGER NOT NULL,
-  availability BOOLEAN DEFAULT TRUE,
-  rate DECIMAL(10, 2) NOT NULL,
+  availability VARCHAR(3) CHECK(availability = 'yes' OR availability = 'no'),
+  room_rate INTEGER NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS review (
-  review_id SERIAL PRIMARY KEY,
-  reviewer_id INTEGER REFERENCES user (user_id),
-  property_id INTEGER REFERENCES property (property_id)
+  review_id INTEGER PRIMARY KEY,
+  reviewer_id INTEGER REFERENCES users (user_id),
+  property_id INTEGER REFERENCES property (property_id),
   rating INTEGER NOT NULL,
   review TEXT,
-  review_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  review_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS booking (
-  booking_ id SERIAL PRIMARY KEY,
+  booking_id INTEGER PRIMARY KEY,
   property_id INTEGER REFERENCES property (property_id),
-  student_id INTEGER REFERENCES user (user_id),
+  student_id INTEGER REFERENCES users (user_id),
   start_date DATE NOT NULL,
   end_date DATE NOT NULL,
-  booking_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-  status VARCHAR(16) CHECK(status = 'processing', status = 'confirmed')
+  booking_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  status VARCHAR(16) CHECK(status = 'processing' OR status = 'confirmed')
 );
 
 CREATE TABLE IF NOT EXISTS transaction (
-  transaction_id SERIAL PRIMARY KEY,
-  booking_id INTEGER REFERENCES bookings (booking_id),
+  transaction_id INTEGER PRIMARY KEY,
+  booking_id INTEGER REFERENCES booking (booking_id),
   amount DECIMAL(10, 2) NOT NULL,
-  status ENUM('pending', 'completed', 'refunded', 'disputed') NOT NULL,
-  datetime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  status VARCHAR CHECK(status = 'processing' OR status = 'paid'),
+  datetime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS administrator (
   user_id INTEGER,
-  password VARCHAR(256),
-  permissions TEXT
-  PRIMARY KEY(user_id, password)
-  FOREIGN KEY(user_id, password) REFERENCES user (user_id, password)
+  password_hash VARCHAR(256),
+  permissions TEXT,
+  PRIMARY KEY(user_id),
+  FOREIGN KEY(user_id) REFERENCES users (user_id)
 );
-
