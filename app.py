cc91028e6c46812ba478bca1ca499448ab1baab8
brendon <e0773745@u.nus.edu>
2023-03-24 00:31:35 +0800

    Update app.py to include signup

--- app.py
+++ app.py
@@ -261,17 +261,14 @@ def login():
         # Call the sign_in function and pass user_id and password
         user_id = request.form['user_id']
         password = request.form['password']
-        resp = make_response()
-        resp.set_cookie('Login', user_id, secure = True)
-        if sign_in(user_id, password):
-            # Redirect to the main page if login is successful
-            return redirect(url_for('home'))
-        else:
+        if True==False:
             # Show an error message if login fails
             error = 'Invalid user ID or password. Please try again.'
             return render_template('login.html', error=error)
-    else:
-        return render_template('login.html')
+        response = redirect(url_for("home"))
+        response.set_cookie('SessionCookie', user_id, secure=True)
+        return response
+    return render_template('login.html')
 
 
 # Create a route for the sign up page
@@ -281,8 +278,22 @@ def signup():
         # Call the sign_up function and pass user_id and password
         user_id = request.form['user_id']
         password = request.form['password']
-        if sign_up(user_id, password):
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone']
+        gender = request.form['gender']
+        age = request.form['age']
+        if sign_up(user_id, password, name, email, phone, gender, age):
             # Redirect to the login page after successful signup
+            try:
+                insert_command = f"INSERT INTO users VALUES({user_id}, {password}, {name}, {email}, {phone}, {gender}, {age});"
+                statement = sqlalchemy.text(insert_command)
+                db.execute(statement)
+                db.commit()
+                return Response(statement.text)
+            except Exception as e:
+                db.rollback()
+                return Response(str(e),403)
             return redirect(url_for('login'))
         else:
             # Show an error message if signup fails
@@ -292,7 +303,7 @@ def signup():
         return render_template('signup.html')
 
 def get_password(user_id):
-    password_query = f'SELECT password_hash FROM users WHERE user_id = {user_id}'
+    password_query = f'SELECT password_hash FROM users WHERE user_id = {user_id};'
     statement = sqlalchemy.text(password_query)
     res = db.execute(statement)
     db.commit()
@@ -307,15 +318,18 @@ def sign_in(user_id, password):
     stored_password= get_password(user_id)
     return password == stored_password
 
-def sign_up(user_id, password):
+def sign_up(user_id, password, name, email, phone, gender, age):
     """
     Takes user_id and password and creates a new user
-    Add fields for email, phone number, gender & age
-    Return True if the user exists and the password is correct otherwise, return False.
+    Add fields for name, email, phone number, gender & age
+    Returns false if the user exists, and true if there's no existing user
     """
-    # Add sql code here
-    user_command = "INSERT INTO users VALUES(user_id, password)"
-    pass
+    user_id_check = f'SELECT user_id FROM users WHERE user_id = {user_id}'
+    res = db.execute(sqlalchemy.text(user_id_check))
+    db.commit()
+    if(list(res) > 0): #user_id exists in the table already
+        return False
+    return True
 
 
 # ? This method can be used by waitress-serve CLI 
@@ -323,11 +337,11 @@ def create_app():
    return app
 
 # ? The port where the debuggable DB management API is served
-PORT = 2222
+PORT = 1234
 # ? Running the flask app on the localhost/0.0.0.0, port 2222
 # ? Note that you may change the port, then update it in the view application too to make it work (don't if you don't have another application occupying it)
 if __name__ == "__main__":
-    app.run("0.0.0.0", PORT)
+    app.run("0.0.0.0", 1234)
     # ? Uncomment the below lines and comment the above lines below `if __name__ == "__main__":` in order to run on the production server
     # ? Note that you may have to install waitress running `pip install waitress`
     # ? If you are willing to use waitress-serve command, please add `/home/sadm/.local/bin` to your ~/.bashrc
---
+++ property.sql
@@ -0,0 +1,7 @@
+/*Populate property table */
+
+INSERT INTO mytable(user_id,password_hash,user_name,email,phone_number,gender,age) VALUES (1,'8','Dael Boulde','20312 Steensland Way','2','false',4311.2);
+INSERT INTO mytable(user_id,password_hash,user_name,email,phone_number,gender,age) VALUES (2,'4','Priscilla Laurenty','87 Atwood Way','5','true',1476.55);
+INSERT INTO mytable(user_id,password_hash,user_name,email,phone_number,gender,age) VALUES (3,'2','Frederico Tierney','4 Pond Alley','1','true',2218.31);
+INSERT INTO mytable(user_id,password_hash,user_name,email,phone_number,gender,age) VALUES (4,'7','Dre Andrzejewski','768 Johnson Center','2','false',4885.03);
+INSERT INTO mytable(user_id,password_hash,user_name,email,phone_number,gender,age) VALUES (5,'3','Bryn Kleszinski','442 Hanson Point','1','true',1664.89);
