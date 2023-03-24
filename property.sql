d2e53541bdf649a477a10e1a650b1d81ec98c194
brendon <e0773745@u.nus.edu>
2023-03-24 10:44:03 +0800

    Modify homepage and include SQL

--- property.sql
+++ property.sql
@@ -1,7 +1,7 @@
 /*Populate property table */
 
-INSERT INTO mytable(user_id,password_hash,user_name,email,phone_number,gender,age) VALUES (1,'8','Dael Boulde','20312 Steensland Way','2','false',4311.2);
-INSERT INTO mytable(user_id,password_hash,user_name,email,phone_number,gender,age) VALUES (2,'4','Priscilla Laurenty','87 Atwood Way','5','true',1476.55);
-INSERT INTO mytable(user_id,password_hash,user_name,email,phone_number,gender,age) VALUES (3,'2','Frederico Tierney','4 Pond Alley','1','true',2218.31);
-INSERT INTO mytable(user_id,password_hash,user_name,email,phone_number,gender,age) VALUES (4,'7','Dre Andrzejewski','768 Johnson Center','2','false',4885.03);
-INSERT INTO mytable(user_id,password_hash,user_name,email,phone_number,gender,age) VALUES (5,'3','Bryn Kleszinski','442 Hanson Point','1','true',1664.89);
+INSERT INTO property(user_id,password_hash,user_name,email,phone_number,gender,age) VALUES (1,'8','Dael Boulde','20312 Steensland Way','2','false',4311.2);
+INSERT INTO property(user_id,password_hash,user_name,email,phone_number,gender,age) VALUES (2,'4','Priscilla Laurenty','87 Atwood Way','5','true',1476.55);
+INSERT INTO property(user_id,password_hash,user_name,email,phone_number,gender,age) VALUES (3,'2','Frederico Tierney','4 Pond Alley','1','true',2218.31);
+INSERT INTO property(user_id,password_hash,user_name,email,phone_number,gender,age) VALUES (4,'7','Dre Andrzejewski','768 Johnson Center','2','false',4885.03);
+INSERT INTO property(user_id,password_hash,user_name,email,phone_number,gender,age) VALUES (5,'3','Bryn Kleszinski','442 Hanson Point','1','true',1664.89);
